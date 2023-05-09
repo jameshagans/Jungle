16be6b91337573924602eeb83305f55824b133b3
jameshagans <jamesy.h95@gmail.com>
2023-05-09 21:06:44 +0000

    Add spec testing in Cypres for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,11 @@
+describe("Visit the home page", () => {
+  beforeEach(() => {
+      cy.visit("/");
+    });
+    it("There are products on the page", () => {
+      cy.get(".products article").should("be.visible");
+    });
+    it("There are 2 products on the page", () => {
+      cy.get(".products article").should("have.length", 2);
+    });
+  });
